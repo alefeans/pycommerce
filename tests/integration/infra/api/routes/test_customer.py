da84ac3e0f64c8d4d925102d53f0ebeede475722
alefeans <alefeans2@hotmail.com>
2023-03-31 15:42:48 -0300

    adjusting tests

--- tests/integration/infra/api/routes/test_customer.py
+++ tests/integration/infra/api/routes/test_customer.py
@@ -1,43 +1,55 @@
+import pytest
 from uuid import uuid4
+
+
+@pytest.fixture
+def create_customer_payload():
+    return {
+        "name": "Test",
+        "email": "test@gmail.com",
+        "password": "password",
+    }
 
 
 async def test_fetch_customer_not_found(async_test_client):
     uid = uuid4()
     async with async_test_client as client:
         response = await client.get(f"/customers/{uid}")
-    assert response.status_code == 404
+        assert response.status_code == 404
 
 
 async def test_fetch_customer_invalid_id(async_test_client):
     async with async_test_client as client:
         response = await client.get("/customers/invalid-id")
+        data, status_code = response.json(), response.status_code
+        assert status_code == 422
+        assert data == {
+            "detail": [
+                {
+                    "loc": ["path", "customer_id"],
+                    "msg": "value is not a valid uuid",
+                    "type": "type_error.uuid",
+                }
+            ]
+        }
 
-    data, status_code = response.json(), response.status_code
-    assert status_code == 422
-    assert data == {
-        "detail": [
-            {
-                "loc": ["path", "customer_id"],
-                "msg": "value is not a valid uuid",
-                "type": "type_error.uuid",
-            }
-        ]
-    }
 
-
-async def test_create_customer_successfully(async_test_client):
-    payload = {
-        "name": "Test",
-        "email": "test@gmail.com",
-        "password": "password",
-    }
+async def test_create_customer_persistence(async_test_client, create_customer_payload):
     async with async_test_client as client:
-        response = await client.post("/customers", json=payload)
+        response = await client.post("/customers", json=create_customer_payload)
         created, status_code = response.json(), response.status_code
         assert status_code == 201
 
         response = await client.get(f'/customers/{created["id"]}')
         data, status_code = response.json(), response.status_code
         assert status_code == 200
-
         assert created["email"] == data["email"]
+
+
+async def test_create_customer_conflict(async_test_client, create_customer_payload):
+    async with async_test_client as client:
+        await client.post("/customers", json=create_customer_payload)
+        response = await client.post("/customers", json=create_customer_payload)
+        data, status_code = response.json(), response.status_code
+        assert status_code == 409
+        assert data == {"detail": "Customer already exists"}
