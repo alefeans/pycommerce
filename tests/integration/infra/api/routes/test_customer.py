ed8aa3502c7764d9ecd11f5dff7af3f9c040a98b
alefeans <alefeans2@hotmail.com>
2023-03-31 12:54:21 -0300

    creating customer api test

--- tests/integration/infra/api/routes/test_customer.py
+++ tests/integration/infra/api/routes/test_customer.py
@@ -1,5 +1,6 @@
 from uuid import uuid4
 
+
 async def test_fetch_customer_not_found(async_test_client):
     uid = uuid4()
     async with async_test_client as client:
@@ -22,3 +23,21 @@ async def test_fetch_customer_invalid_id(async_test_client):
             }
         ]
     }
+
+
+async def test_create_customer_successfully(async_test_client):
+    payload = {
+        "name": "Test",
+        "email": "test@gmail.com",
+        "password": "password",
+    }
+    async with async_test_client as client:
+        response = await client.post("/customers", json=payload)
+        created, status_code = response.json(), response.status_code
+        assert status_code == 201
+
+        response = await client.get(f'/customers/{created["id"]}')
+        data, status_code = response.json(), response.status_code
+        assert status_code == 200
+
+        assert created["email"] == data["email"]
