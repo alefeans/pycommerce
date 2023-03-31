c3d856cad91ab568cee4e7b02c890f167b6f9909
alefeans <alefeans2@hotmail.com>
2023-03-31 11:33:36 -0300

    adjusting project structure

---
+++ pycommerce/infra/db/__init__.py
@@ -0,0 +1,14 @@
+from typing import AsyncGenerator
+from sqlmodel.ext.asyncio.session import AsyncSession
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.asyncio import create_async_engine
+from pycommerce.config import get_settings
+
+DBSession = AsyncSession
+engine = create_async_engine(get_settings().DB_URL, future=True)
+
+
+async def get_session() -> AsyncGenerator[DBSession, None]:
+    async_session = sessionmaker(engine, class_=DBSession, expire_on_commit=False)
+    async with async_session() as session:
+        yield session
