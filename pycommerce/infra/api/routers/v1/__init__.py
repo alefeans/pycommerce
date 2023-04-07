00286da105a20de5f7a5b06ba03f2705bdfe100c
alefeans <alefeans2@hotmail.com>
2023-04-07 18:57:11 -0300

    adding auth routes

--- pycommerce/infra/api/routers/v1/__init__.py
+++ pycommerce/infra/api/routers/v1/__init__.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter
+from .auth import router as auth_router
 from .user import router as user_router
 
 router = APIRouter()
+router.include_router(auth_router, prefix="/auth", tags=["Auth"])
 router.include_router(user_router, prefix="/users", tags=["Users"])
---
+++ pycommerce/infra/api/routers/v1/auth.py
@@ -0,0 +1,44 @@
+from fastapi import APIRouter, HTTPException
+from pycommerce.infra.api.dependencies.auth import (
+    CurrentUser,
+    OauthForm,
+    Token,
+    create_access_token,
+)
+from pycommerce.core.entities.user import UserResponse
+from pycommerce.core.services.user import authenticate
+from pycommerce.infra.api.dependencies.repositories import UserRepo
+from pycommerce.infra.api.dependencies.crypto import Hasher
+
+router = APIRouter()
+
+
+@router.post(
+    "/token",
+    summary="Create Oauth Token",
+    responses={
+        200: {"description": "User authenticated"},
+        401: {"description": "User unauthorized"},
+    },
+)
+async def token(repo: UserRepo, hasher: Hasher, form_data: OauthForm) -> Token:
+    user = await authenticate(repo, hasher, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=401,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    return create_access_token({"sub": f"user_id:{user.id}"})
+
+
+@router.get(
+    "/me",
+    summary="Get authenticated User information",
+    responses={
+        200: {"description": "User info"},
+        401: {"description": "User unauthorized"},
+    },
+)
+async def read_users_me(current_user: CurrentUser) -> UserResponse:
+    return current_user
