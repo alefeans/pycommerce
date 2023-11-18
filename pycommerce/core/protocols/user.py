09f0d5769e8d06e5bff26f2f590f43532ec70645
alefeans <alefeans2@hotmail.com>
2023-11-17 20:41:17 -0800

    Refactor user protocols

--- pycommerce/core/protocols/user.py
+++ pycommerce/core/protocols/user.py
@@ -1,21 +1,32 @@
+from typing import Optional, Protocol
 from uuid import UUID
-from typing import Protocol, Optional
-from pydantic import EmailStr
-from pycommerce.core.entities.user import User, UpdateUserDTO
+
+from pycommerce.core.entities.user import Email, Password, User
+from pycommerce.core.protocols.crypto import Hasher
+from pycommerce.core.protocols.unit_of_work import UnitOfWork
 
 
 class UserRepo(Protocol):
     async def persist(self, user: User) -> User:
         ...
 
-    async def fetch_by_email(self, email: EmailStr) -> Optional[User]:
+    async def get_by_email(self, email: Email) -> Optional[User]:
         ...
 
-    async def fetch_by_id(self, _id: UUID) -> Optional[User]:
+    async def get_by_id(self, _id: UUID) -> Optional[User]:
         ...
 
     async def delete(self, _id: UUID) -> bool:
         ...
 
-    async def update(self, _id: UUID, dto: UpdateUserDTO) -> Optional[User]:
+    async def update(self, user: User) -> User:
+        ...
+
+
+class UserUnitOfWork(UnitOfWork, Protocol):
+    user_repo: UserRepo
+
+
+class UserHasher(Hasher, Protocol):
+    def hash(self, value: str) -> Password:
         ...
