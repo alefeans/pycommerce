5653d69955eb9ed758a0ac6a2b6c4e4ad8dd9e1a
alefeans <alefeans2@hotmail.com>
2023-11-17 20:39:43 -0800

    Remove external dependencies from User entity

--- pycommerce/core/entities/user.py
+++ pycommerce/core/entities/user.py
@@ -1,38 +1,37 @@
+import re
+from dataclasses import dataclass, field
+from typing import NewType
 from uuid import UUID, uuid4
-from typing import Optional
-from pydantic import BaseModel, Field, EmailStr
+
+from pycommerce.core.entities.base import Entity
 
 
-ID = Field(default_factory=uuid4)
-Password = Field(..., min_length=8, max_length=100)
+class InvalidUser(Exception):
+    pass
 
 
-class User(BaseModel):
-    id: UUID = ID
+Password = NewType("Password", str)
+Email = NewType("Email", str)
+
+
+@dataclass(frozen=True)
+class User(Entity):
     name: str
-    email: EmailStr
-    password: str = Password
+    email: Email
+    password: Password
+    id: UUID = field(default_factory=uuid4)
 
-    class Config:
-        allow_mutation = False
-        orm_mode = True
+    def validate(self):
+        self.validate_email(self.email)
+        self.validate_password(self.password)
 
+    @staticmethod
+    def validate_email(email: str) -> None:
+        email_pattern = r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
+        if not re.match(email_pattern, email):
+            raise InvalidUser(f"Invalid email address {email}")
 
-class CreateUserDTO(BaseModel):
-    name: str
-    email: EmailStr
-    password: str = Password
-
-
-class UpdateUserDTO(BaseModel):
-    name: Optional[str]
-    email: Optional[EmailStr]
-
-
-class UserResponse(BaseModel):
-    id: UUID = ID
-    name: str
-    email: EmailStr
-
-    class Config:
-        allow_mutation = False
+    @staticmethod
+    def validate_password(password: str) -> None:
+        if not (8 <= len(password) <= 100):
+            raise InvalidUser("Password size must be between 8 and 100 characters")
