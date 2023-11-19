bbb057a33b995d301fb881e9c3cedf63e7b1c90a
alefeans <alefeans2@hotmail.com>
2023-11-18 21:12:20 -0800

    Remove unecessary generator function

--- pycommerce/infra/db/__init__.py
+++ pycommerce/infra/db/__init__.py
@@ -1,5 +1,3 @@
-from typing import AsyncGenerator
-
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 from sqlmodel.ext.asyncio.session import AsyncSession
 
@@ -8,8 +6,3 @@ from pycommerce.config import get_settings
 DBSession = AsyncSession
 engine = create_async_engine(get_settings().DB_URL)
 async_session = async_sessionmaker(engine, class_=DBSession, expire_on_commit=False)
-
-
-async def get_session() -> AsyncGenerator[DBSession, None]:
-    async with async_session() as session:
-        yield session
