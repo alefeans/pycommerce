61db33f79887d57356f16f4a5b05dd379623ac91
alefeans <alefeans2@hotmail.com>
2023-11-18 21:10:41 -0800

    Remove unecessary fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,6 @@
 import pytest
-from unittest.mock import AsyncMock
+
 from pycommerce.config import get_settings
-
-
-@pytest.fixture
-def async_mocker():
-    return lambda name, spec: AsyncMock(name=name, spec=spec)
 
 
 @pytest.fixture
