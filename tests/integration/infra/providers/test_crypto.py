9da5a479ee8168a70e687d8e80057b0c713b17d5
alefeans <alefeans2@hotmail.com>
2023-11-18 21:09:50 -0800

    Refactor crypto module

--- tests/integration/infra/providers/test_crypto.py
+++ tests/integration/infra/providers/test_crypto.py
@@ -1,18 +1,21 @@
-from pycommerce.infra.providers.crypto import hasher
+from pycommerce.infra.providers.crypto import Hasher
 
 
 def test_if_hashes_value():
+    hasher = Hasher()
     value = "testing"
     assert value != hasher.hash(value)
 
 
 def test_if_returns_true_when_checking_hashed_value():
+    hasher = Hasher()
     value = "testing"
     hashed = hasher.hash(value)
     assert hasher.verify(value, hashed)
 
 
 def test_if_returns_false_when_hashed_value_is_different():
+    hasher = Hasher()
     value = "testing"
     hashed = hasher.hash("different")
     assert not hasher.verify(value, hashed)
